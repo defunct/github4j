866013e8e8ffe2c763a39c9c6c17afb3a7747bc8
Alan Gutierrez <alan@blogometer.com>
2010-06-09 20:46:02 -0500

    Update Mix build after refactor of Mix.

--- src/mix/java/com/goodworkalan/github/downloads/mix/GitHub4JDownloadsProject.java
+++ src/mix/java/com/goodworkalan/github/downloads/mix/GitHub4JDownloadsProject.java
@@ -2,7 +2,7 @@ package com.goodworkalan.reflective.mix;
 
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
-import com.goodworkalan.mix.builder.JavaProject;
+import com.goodworkalan.mix.cookbook.JavaProject;
 
 /**
  * Builds the project definition for GitHub4J Downloads.
